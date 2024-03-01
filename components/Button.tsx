75348eb81896e5c295611d8ecd2df2c7006fc7d3
Aayush Vaishnav <aayushdoesdev@gmail.com>
2024-03-01 12:36:25 +0530

    completed ui

---
+++ components/Button.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image"
+
+type ButtonProps = {
+    type :'button' | 'submit',
+    title: string
+    icon?: string
+    varient: string
+    full?: boolean
+}
+
+
+const Button = ({type, title, icon, varient, full}: ButtonProps) => {
+  return (
+    <div>
+      <button type={type} 
+        className={`flexCenter gap-3 rounded-full border ${varient} ${full && "w-full"}`}
+      >
+        {icon && <Image src={icon} alt={title} width={24} height={24}/>}
+        <label className="bold-16 whitespace-nowrap cursor-pointer">{title}</label>
+      </button>
+    </div>
+  )
+}
+
+export default Button
